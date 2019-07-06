abcc1d01406b266277febbc36ea20a303eb37e64
Calvin <calmoon808@gmail.com>
2019-07-06 10:41:24 -1000

    fibby

--- even_fib_nums.js
+++ even_fib_nums.js
@@ -7,23 +7,42 @@
  */
 function _sumFibs( maxFibValue ) {
   let sum = 0;
+  let arr = [0, 1];
 
   // do your work here
-
+  for (let i = 2; i < maxFibValue; i++){
+    if (arr[arr.length - 1] + arr[arr.length - 2] <= maxFibValue){ 
+      arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
+      if (arr[arr.length - 1] % 2 === 0){
+        sum += arr[arr.length - 1];
+      }
+    }
+  }
   return sum;
 }
 
 // bonus round
 function _highestFibonacciNumber (maxFibValue){
   let highest = 0;
+  let arr = [0, 1];
 
   //define your base case, validate your input
+  // for (let i = 2; i < maxFibValue; i++){
+  //   if (arr[arr.length - 1] + arr[arr.length - 2] <= maxFibValue){ 
+  //     arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
+  //   }
+  //   highest = arr[arr.length - 1];
+  // }
+  // return highest;
 
-
-  //do your work here
-
-  return highest;
+  //solve recursively
+    if (maxFibValue <= 1){
+      return 1;
+    }
+    return _highestFibonacciNumber(maxFibValue - 1) + _highestFibonacciNumber(maxFibValue - 2);
 };
+
+console.log(_highestFibonacciNumber(10));
 
 /**
  * Do not modify code below.
